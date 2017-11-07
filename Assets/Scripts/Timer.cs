d8dcdc93ffe97fdbd0324a762b4b5b50711e1a9d
sox2020 <32762690+sox2020@users.noreply.github.com>
2017-11-08 08:09:37 +1100

    added Timer to LV1

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text timer;
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		time += Time.deltaTime;
+		timer.text = "Time: " + time;
+	}
+}
